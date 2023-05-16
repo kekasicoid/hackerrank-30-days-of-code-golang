1e716bec5109b4b712ce116f46084c7923209740
Arditya Kekasi <arditya.kekasi@gmail.com>
2023-05-17 06:57:30 +0700

    Day 6

---
+++ Day-6/main.go
@@ -0,0 +1,59 @@
+package main
+
+import (
+	"bufio"
+	"fmt"
+	"os"
+	"strconv"
+	"strings"
+)
+
+func main() {
+	//Enter your code here. Read input from STDIN. Print output to STDOUT
+	// Read from stdin
+	reader := bufio.NewReader(os.Stdin)
+	s, err := reader.ReadString('\n')
+	if err != nil {
+		fmt.Println(err)
+	}
+
+	s = strings.Trim(s, "\n")
+	s = strings.Trim(s, "\r")
+
+	n, err := strconv.Atoi(s)
+	if err != nil {
+		fmt.Println(err)
+	}
+	evens := []string{}
+	odds := []string{}
+	results := []string{}
+	var spaced string
+	var e string
+	var o string
+
+	// Check the index of input data and assign
+	// even indexes to evens slice, odd to odds
+	// then concatenate and format for output
+	for i := 0; i < n; i++ {
+		s, _ := reader.ReadString('\n')
+		s = strings.Trim(s, "\n")
+
+		for idx, val := range s {
+			if idx%2 == 0 {
+				evens = append(evens, string(val))
+			} else {
+				odds = append(odds, string(val))
+			}
+		}
+		e = strings.Join(evens, "")
+		o = strings.Join(odds, "")
+		tmp := []string{e, o}
+		spaced = strings.Join(tmp, " ")
+		results = append(results, spaced)
+		evens = []string{}
+		odds = []string{}
+	}
+
+	result := strings.Join(results, "\n")
+	fmt.Println(result)
+}
